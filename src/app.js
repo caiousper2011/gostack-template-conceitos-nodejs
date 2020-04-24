7d283bd9dbb0e37edee2f671248dd018d3c92db9
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2020-04-24 04:03:19 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = [
+  {
+    "id": "f43518d3-163e-4ee7-9aa6-0becf8ad6a16",
+    "likes": 0,
+    "title": "Desafio Node.js",
+    "url": "http://github.com/...",
+    "techs": [
+      "Node.js"
+    ]
+  }
+];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  response.json(request.body);
+  repositories.push({id: uuid(), likes: 0, ...request.body})
 });
 
 app.put("/repositories/:id", (request, response) => {
